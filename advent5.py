aa4d1c60ab866ecd32436dd904e615d7a89e5459
Victor Gambier <vgambier@oxyl.fr>
2022-12-06 11:31:22 +0100

    turn crate strings into usable array

--- advent5.py
+++ advent5.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-def contains_number():
-  print("hey")
-
 def parse():
 
   crates = []
@@ -15,9 +12,23 @@ def parse():
       else:
         crates.append(line.strip('\n'))
       
-  return crates[:-1], instructions[1:]
+  return crates[:-1], instructions[1:], int(instructions[0][-2])
+
+def improve_crates(crates, nb_crates):
+  data_crates = [[] for i in range(nb_crates)]
+  for crate_line in crates:
+    for i in range(nb_crates):
+      candidate = crate_line[i*4+1]
+      print(candidate)
+      if candidate != ' ':
+        data_crates[i].append(candidate)
+  return data_crates
 
 if __name__ == '__main__':
-  crates, instructions = parse()
-  print(crates)
-  print(instructions)
+
+  crates, instructions, nb_crates = parse()
+  
+  # modify data structure for crates
+  data_crates = improve_crates(crates, nb_crates)
+  print(data_crates)
+
